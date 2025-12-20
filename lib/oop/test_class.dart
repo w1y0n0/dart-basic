2b6cc1e9a4b4c663bc33cf35ec042a931b9c7021
w1y0n0 <wiyono30@gmail.com>
2025-12-20 08:40:57 +0700

    Extentions Methods

--- lib/oop/test_class.dart
+++ lib/oop/test_class.dart
@@ -1,4 +1,5 @@
-import 'package:dart_basic_tutorial/oop/mobil.dart';
+import 'package:dart_basic_tutorial/oop/extension_sample.dart';
+// import 'package:dart_basic_tutorial/oop/mobil.dart';
 
 void main() {
   // Mobil mobil = Mobil('Toyota', 'Sedan', 2010);
@@ -10,7 +11,13 @@ void main() {
   // Mobil mobilHonda = Mobil('Honda', 'Sedan', 2015);
   // print(mobilHonda.tahun);
 
-  Mobil mobil = Mobil(merk: 'Toyota', model: 'Sport', tahun: 2020);
-  mobil.klakson();
-  print('Mobil merk ${mobil.merk}. Model ${mobil.model}. Tahun ${mobil.tahun}');
+  // Mobil mobil = Mobil(merk: 'Toyota', model: 'Sport', tahun: 2020);
+  // mobil.klakson();
+  // print('Mobil merk ${mobil.merk}. Model ${mobil.model}. Tahun ${mobil.tahun}');
+
+  String data = '10';
+  int newData = int.parse(data);
+  int freshData = data.parseInt();
+  print('Data: $newData');
+  print('Fresh Data: $freshData');
 }
